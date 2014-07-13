c8a78aa02f2ee0b015dbc93c5bcaea790d6cd0f3
Somnid <>
2014-07-12 21:58:24 -0600

    creating filesystem module

--- js/file-system.js
+++ js/file-system.js
@@ -1,3 +1,32 @@
 var FileSystem = (function(){
-  
-});
+  function getUserFolder(){
+    return new Promise(function(resolve, reject){
+      chrome.storage.local.get("folderRef", function(results){
+        if(results.folderRef){
+          retrieveFolder(results.folderRef).then(resolve);
+        }else{
+          askForFolder().then(resolve);
+        }
+      });
+    });
+  }
+  function askForFolder(){
+    return new Promise(function(resolve, reject){
+      chrome.fileSystem.chooseEntry({ type : "openDirectory" }, function(entry, fileEntries){
+        var entryId = chrome.fileSystem.retainEntry(entry);
+        chrome.storage.local.set({ folderRef :  entryId });
+        resolve(entry, entryId);
+      });
+    });
+  }
+  function retrieveFolder(id){
+    return new Promise(function(resolve, reject){
+      chrome.fileSystem.restoreEntry(id, function(entry){
+        resolve(entry);
+      });
+    });
+  }
+  return {
+    getUserFolder : getUserFolder
+  };
+})();
--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var startButton = document.getElementById("btn-start");
 	var killButton = document.getElementById("btn-kill");
+	var locationButton = document.getElementById("btn-location");
 	var server;
-	
+	var fsRoot;
+
 	startButton.addEventListener("click", function(){
+	  if(!fsRoot){
+	    console.log("setup location first");
+	    return;
+	  }
 		startButton.disabled = true;
 		killButton.disabled = false;
 		server = HttpServer.create({
@@ -11,9 +17,14 @@ document.addEventListener("DOMContentLoaded", function(){
 			ip : "127.0.0.1",
 			onRequest : function(request){
 				console.log("reading info", { request : request });
-				
-				return Ajax.promiseRequest({
-					url : "file:///C:/Users/dkumagai1/Desktop/ext/ChromeServer/manifest.json"
+
+				return new Promise(function(resolve, reject){
+				  fsRoot.getFile(request.uri, { create : false }, function(file){
+				    console.log(file);
+				    resolve(file);
+				  },function(error){
+				    reject(error);
+				  });
 				});
 			},
 			onKill : function(){
@@ -22,10 +33,20 @@ document.addEventListener("DOMContentLoaded", function(){
 			}
 		});
 	}, true);
-	
+
 	killButton.addEventListener("click", function(){
 		startButton.disabled = true;
 		killButton.disabld = false;
 		server.kill();
 	}, true);
+
+	locationButton.addEventListener("click", function(){
+		FileSystem.getUserFolder().then(function(entry){
+		  fsRoot = entry;
+		});
+	}, true);
+
+	FileSystem.getUserFolder().then(function(entry){
+		fsRoot = entry;
+	});
 }, true);

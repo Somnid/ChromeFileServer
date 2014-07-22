ed85576537d23ff6c3cdd0129434220a657e3fa6
Doug <>
2014-07-22 09:48:30 -0600

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(){
   ServerView.create();
 }, true);
-
-function getStuff(url) {
-    FileSystem.getUserFolder().then(function(entry) {
-        entry.getFile(url, {create: false}, function(file) {
-            console.log(file);
-        }, function(error) {
-            console.log(error);
-        });
-    })
-}
-
-function showStuff(url) {
-    FileSystem.getUserFolder().then(function(entry) {
-      reader = entry.createReader();
-      reader.readEntries(function(results){
-        console.log(results);
-      });
-    });
-}
